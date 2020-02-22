76a16cd394092ad8f27ed4336f792ddf7e87155d
Majkl <majkl.andracic@gmail.com>
2020-02-22 17:00:20 +0100

    Workout

--- app/src/main/java/com/example/boxingstopwatch/Workout.java
+++ app/src/main/java/com/example/boxingstopwatch/Workout.java
@@ -1,4 +1,34 @@
 package com.example.boxingstopwatch;
 
 public class Workout {
+
+    private String name;
+    private String description;
+
+
+    public static final Workout[] workouts = {
+         new Workout("The Limb Loosener","5 Handstand push-ups\10 " +
+                             "1-legged squats\n15 Pull-ups"),
+
+
+    public Workout(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
