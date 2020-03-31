ad4c9fb51a9625ab2c5eceee7662372f4bdc1c2e
Majkl <majkl.andracic@gmail.com>
2020-03-31 23:32:39 +0200

    Workout detail fragment update

--- app/src/main/java/com/example/boxingstopwatch/WorkoutDetailFragment.java
+++ app/src/main/java/com/example/boxingstopwatch/WorkoutDetailFragment.java
@@ -4,11 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class WorkoutDetailFragment extends Fragment {
-
     private long workoutId;
 
     @Override
@@ -16,5 +16,20 @@ public class WorkoutDetailFragment extends Fragment {
                              Bundle savedInstanceState){
 
         return inflater.inflate(R.layout.fragment_workout_detail, container,false);
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        View view = getView();
+        if(view != null){
+            TextView title = view.findViewById(R.id.textTitle);
+            Workout workout = Workout.workouts[(int)workoutId];
+            title.setText(workout.getName());
+            TextView description = view.findViewById(R.id.textDescription);
+            description.setText(workout.getDescription());
+        }
+    }
+    public void setWorkout(long id){
+        this.workoutId = id;
     }
 }
