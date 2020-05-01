a336e44c89a989001df6107825002fdc47cefeab
Majkl <majkl.andracic@gmail.com>
2020-05-01 23:47:10 +0200

    Adding StopwatchFragment

--- app/src/main/java/com/example/boxingstopwatch/StopwatchActivity.java
+++ app/src/main/java/com/example/boxingstopwatch/StopwatchActivity.java
@@ -13,9 +13,6 @@ import java.util.Locale;
 
 public class StopwatchActivity extends AppCompatActivity implements WorkoutListFragment.Listener{
 
-    private int seconds = 0;
-    private boolean running;
-    private boolean wasRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,69 +21,13 @@ public class StopwatchActivity extends AppCompatActivity implements WorkoutListF
 
 
 
-        if(savedInstanceState != null){
-            seconds = savedInstanceState.getInt("seconds");
-            running = savedInstanceState.getBoolean("running");
-            wasRunning = savedInstanceState.getBoolean("wasRunning");
-        }
-        runTimer();
+
 
         WorkoutDetailFragment workoutDetailFragment = new WorkoutDetailFragment();
         FragmentManager manager = getSupportFragmentManager();
         manager.beginTransaction()
                 .replace(R.id.workout_detail, workoutDetailFragment, workoutDetailFragment.getTag())
                 .commit();
-    }
-    @Override
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-        super.onSaveInstanceState(savedInstanceState);
-        savedInstanceState.putInt("seconds", seconds);
-        savedInstanceState.putBoolean("running", running);
-        savedInstanceState.putBoolean("wasRunning", wasRunning);
-    }
-    @Override
-    protected void onPause(){
-        super.onPause();
-        wasRunning = running;
-        running = false;
-    }
-    @Override
-    protected void onResume(){
-        super.onResume();
-        if(wasRunning){
-            running = true;
-        }
-    }
-    public void onClickStart(View view){
-        running = true;
-    }
-    public void onClickStop(View view){
-        running = false;
-    }
-    public void onClickReset(View view){
-        running = false;
-        seconds = 0;
-    }
-    private void runTimer() {
-        final TextView timeView = findViewById(R.id.time_view);
-        final Handler handler = new Handler();
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                int hours = seconds / 3600;
-                int minutes = (seconds % 3600) / 60;
-                int secs = seconds % 60;
-                String time = String.format(Locale.getDefault(),
-                        "%d:%02d:%02d", hours, minutes, secs);
-                timeView.setText(time);
-                if (running) {
-                    seconds++;
-                }
-                handler.postDelayed(this, 1000);
-
-            }
-
-        });
     }
 
     @Override
---
+++ app/src/main/java/com/example/boxingstopwatch/StopwatchFragment.java
@@ -0,0 +1,90 @@
+package com.example.boxingstopwatch;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import java.util.Locale;
+
+public class StopwatchFragment extends Fragment {
+
+    private int seconds = 0;
+    private boolean running;
+    private boolean wasRunning;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
+        }
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        View layout = inflater.inflate(R.layout.fragment_stopwatch, container, false);
+        runTimer(layout);
+        return layout;
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        if(wasRunning){
+            running = true;
+        }
+    }
+    public void onClickStart(View view){
+        running = true;
+    }
+    public void onClickStop(View view){
+        running = false;
+    }
+    public void onClickReset(View view){
+        running = false;
+        seconds = 0;
+    }
+    private void runTimer(View view) {
+        final TextView timeView = view.findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+                String time = String.format(Locale.getDefault(),
+                        "%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+
+            }
+
+        });
+    }
+
+}
+
